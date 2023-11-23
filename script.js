320fb846f131be3b83f9c34fe32e540296094eb0
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-23 10:54:11 +0300

    add increment function

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //document.getElementById("count-el").innerText = 5
 
+// initialize count to 0
+let count = 0
+// listen for clicks
+function increment() {
+    count += 1
+    document.getElementById("count-el").innerText = count
+}
+
