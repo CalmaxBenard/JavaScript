2d17e84159776aaec5cb90091ce1e0b4e8f3d2c7
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-23 11:52:28 +0300

    save previous elements

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-//document.getElementById("count-el").innerText = 5
+//get html elements
+let saveEl = document.getElementById("save_el")
+let countEL = document.getElementById("count-el")
 
 // initialize count to 0
 let count = 0
+
 // listen for clicks
 function increment() {
     count += 1
-    document.getElementById("count-el").innerText = count
+    countEL.innerHTML = count
+}
+
+function save() {
+    let counterOutput = count + " - "
+    saveEl.innerHTML += counterOutput
+    count = 0
+    countEL.innerHTML = count
 }
 
